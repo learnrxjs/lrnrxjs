b501b6839adf71aff3907583eec13e04e53d8bbf
Sam Bulatov <mephistorine@gmail.com>
2022-12-28 16:46:56 +0300

    fix: Битые ссылки + не правильное название лейаута

--- projects/lrnrxjs-core/src/data/sidebars.ts
+++ projects/lrnrxjs-core/src/data/sidebars.ts
@@ -8,10 +8,11 @@ export const mainSidebarMenu: readonly MenuItem[] = [
     links: [
       { type: "LINK", title: "Введение", url: "/guide/overview" },
       { type: "LINK", title: "Потоки", url: "/guide/observables" },
+      { type: "LINK", title: "Операторы", url: "/guide/operators" },
       { type: "LINK", title: "Наблюдатель", url: "/guide/observer" },
       { type: "LINK", title: "Подписки", url: "/guide/subscription" },
-      { type: "LINK", title: "Сабжекты", url: "/guide/subject" },
-      { type: "LINK", title: "Шедулеры", url: "/guide/scheduler" },
+      { type: "LINK", title: "Сабжекты", url: "/guide/subjects" },
+      { type: "LINK", title: "Шедулеры", url: "/guide/schedulers" },
       { type: "LINK", title: "Marble тестирование", url: "/guide/testing/marble-testing" },
     ]
   },
